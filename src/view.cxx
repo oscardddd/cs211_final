672916984e89a628350397eb5414229c68f48cf2
Bruce Zhu <bruce.zhekang.zhu@gmail.com>
2022-11-30 11:50:05 -0500

    Update view.cxx

--- src/view.cxx
+++ src/view.cxx
@@ -32,7 +32,7 @@ View::draw(ge211::Sprite_set& set)
                                          grid_size * i};
             set.add_sprite(background_sprite_, grid_pos, 1,
                            ge211::Transform().set_scale(0.28));
-            // add_piece_sprite_(set, {i,j});
+            add_piece_sprite_(set, {j,i});
         }
     }
 }
@@ -65,27 +65,27 @@ std::string View::initial_window_title() const
 void
 View::add_piece_sprite_(ge211::Sprite_set& set, Position posn) const
 {
-    Position pos = {grid_size*posn.x,grid_size*posn.y};
+    Position pos = {grid_size * posn.x,grid_size * posn.y};
     if (model_[posn] == 1) {
-        set.add_sprite(soldier_red_sprite_, pos, 2);
+        set.add_sprite(soldier_red_sprite_, pos, 3);
     } else if (model_[posn] == 2) {
-        set.add_sprite(cannon_red_sprite_, pos, 2);
+        set.add_sprite(cannon_red_sprite_, pos, 3);
     } else if (model_[posn] == 3) {
-        set.add_sprite(car_red_sprite_, pos, 2);
+        set.add_sprite(car_red_sprite_, pos, 3);
     } else if (model_[posn] == 4) {
-        set.add_sprite(horse_red_sprite_, pos, 2);
+        set.add_sprite(horse_red_sprite_, pos, 3);
     } else if (model_[posn] == 5) {
-        set.add_sprite(elephant_red_sprite_, pos, 2);
+        set.add_sprite(elephant_red_sprite_, pos, 3);
     } else if (model_[posn] == 6) {
-        set.add_sprite(advisor_red_sprite_, pos, 2);
+        set.add_sprite(advisor_red_sprite_, pos, 3);
     } else if (model_[posn] == 7) {
-        set.add_sprite(general_red_sprite_, pos, 2);
+        set.add_sprite(general_red_sprite_, pos, 3);
     } else if (model_[posn] == 11) {
-        set.add_sprite(soldier_black_sprite_, pos, 2);
+        set.add_sprite(soldier_black_sprite_, pos, 3);
     } else if (model_[posn] == 12) {
-        set.add_sprite(cannon_black_sprite_, pos, 2);
+        set.add_sprite(cannon_black_sprite_, pos, 3);
     } else if (model_[posn] == 13) {
-        set.add_sprite(car_black_sprite_, pos, 2);
+        set.add_sprite(car_black_sprite_, pos, 3);
     } else if (model_[posn] == 14) {
         set.add_sprite(horse_black_sprite_, pos, 2);
     } else if (model_[posn] == 15) {
@@ -94,5 +94,7 @@ View::add_piece_sprite_(ge211::Sprite_set& set, Position posn) const
         set.add_sprite(advisor_black_sprite_, pos, 2);
     } else if (model_[posn] == 17) {
         set.add_sprite(general_black_sprite_, pos, 2);
+    } else {
+        set.add_sprite(general_black_sprite_, pos, 2, ge211::Transform().set_scale(0.28));
     }
 }
